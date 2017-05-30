dbd17548a02592cf31534ce8e9269a80e2001a25
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-05-30 21:38:48 +0530

    working on twitch app. basic frontend framework made

---
+++ Twich Stream App/script.js
@@ -0,0 +1,14 @@
+var base_url = "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?"
+
+var twitch_users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+$(document).ready(function(){
+   
+    
+    $.getJSON(base_url,function(data){
+       
+        
+        
+    });
+    
+});
